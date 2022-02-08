405f7e7c2342924e00d424c4979a1018f6ca3a60
francechalon <92053410+francechalon@users.noreply.github.com>
2022-02-08 17:19:19 +0000

    Adda second test for H3

--- crime_app/test_crime.py
+++ crime_app/test_crime.py
@@ -1,6 +1,4 @@
-from curses import window
 from dash.testing.application_runners import import_app
-from selenium.webdriver.support.ui import WebDriverWait
 
 # Change to test_"2 digits of module""2 digits of file"001_"what the test is doing"
 
@@ -13,21 +11,20 @@ def test_crda001_H1_text_equals(dash_duo):
     dash_duo.start_server(app)
     dash_duo.wait_for_element("H1", timeout = 4)
     h1_text = dash_duo.find_element("H1").text
-    assert h1_text.casefold() == "London Crime Rate".casefold()
+    assert h1_text.casefold() == "London Crime Rate Dashboard".casefold()
 
 
+def test_crda002_show_settings(dash_duo):
+    """GIVEN that the app is running
+    WHEN we access the homepage of the app
+    THEN there should be a paragraph with Select Chart Type
+    """
 
-
-"""
-def test_crcr002_open_preview(dash_duo):
-    GIVEN that the homepage is open
-    WHEN we click on a preview of a figure
-    THEN the figure is opened on a seperate webpage
-
-    app = import_app(app_file = "crime_app.app")
+    app = import_app(app_file = "crime_app.dash_app")
     dash_duo.start_server(app)
-    dash_duo.wait_for_element("figure 1", timeout = 4)   # See what figure ID is after 
-    button = dash_duo.find_element_by_id("ADD THE ID NB")
-    action = button.click() 
-    result = open
-"""
+    dash_duo.wait_for_element("H1", timeout = 4)
+    filter_selector = dash_duo.find_element("H3").text
+    assert filter_selector.casefold() == "Display Settings".casefold()
+
+
+
