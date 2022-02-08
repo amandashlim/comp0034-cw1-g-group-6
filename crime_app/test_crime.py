a3d7ad41fe5fc3198ea466fe0eddeea45fe2055b
francechalon <92053410+francechalon@users.noreply.github.com>
2022-02-08 12:41:19 +0000

    Created one test

--- crime_app/test_crime.py
+++ crime_app/test_crime.py
@@ -4,22 +4,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 # Change to test_"2 digits of module""2 digits of file"001_"what the test is doing"
 
-def test_crcr001_H1_text_equals(dash_duo):
+def test_crda001_H1_text_equals(dash_duo):
     """GIVEN that the app is running
     WHEN we access the homepage of the app
-    THEN the H1 heading should say "Crime rates in London boroughs"""
+    THEN the H1 heading should say "London Crime Rates"""
 
-    app = import_app(app_file = "crime_app.app")
+    app = import_app(app_file = "crime_app.dash_app")
     dash_duo.start_server(app)
-    dash_duo.wait_for_element("h1", timeout = 4)
-    h1_text = dash_duo.find_element("h1").text
-    assert h1_text.casefold() == "Crime rates in London boroughs"
+    dash_duo.wait_for_element("H1", timeout = 4)
+    h1_text = dash_duo.find_element("H1").text
+    assert h1_text.casefold() == "London Crime Rate".casefold()
 
 
+
+
+"""
 def test_crcr002_open_preview(dash_duo):
-    """GIVEN that the homepage is open
+    GIVEN that the homepage is open
     WHEN we click on a preview of a figure
-    THEN the figure is opened on a seperate webpage"""
+    THEN the figure is opened on a seperate webpage
 
     app = import_app(app_file = "crime_app.app")
     dash_duo.start_server(app)
@@ -27,4 +30,4 @@ def test_crcr002_open_preview(dash_duo):
     button = dash_duo.find_element_by_id("ADD THE ID NB")
     action = button.click() 
     result = open
-
+"""
