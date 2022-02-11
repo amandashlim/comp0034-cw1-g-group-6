31ff0c79ff9e9fea85cf6e184a9f20df79df43a7
francechalon <92053410+francechalon@users.noreply.github.com>
2022-02-11 15:49:14 +0000

    create 3rd test?

--- crime_app/test_crime.py
+++ crime_app/test_crime.py
@@ -1,4 +1,5 @@
 from dash.testing.application_runners import import_app
+from selenium.webdriver.support.ui import WebDriverWait
 
 # Change to test_"2 digits of module""2 digits of file"001_"what the test is doing"
 
@@ -25,6 +26,23 @@ def test_crda002_show_settings(dash_duo):
     dash_duo.wait_for_element("H1", timeout = 4)
     filter_selector = dash_duo.find_element("H3").text
     assert filter_selector.casefold() == "Display Settings".casefold()
+
+def test_crda003_default_crime(dash_duo):
+    """GIVEN that you launch the app and you land on the homepage
+    WHEN look at the Select Crime to Display filter
+    THEN the default crime should be Burglary"""
+
+    app = import_app(app_file="crime_app.dash_app")
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("H1", timeout=4)
+    WebDriverWait(dash_duo.driver, 3)
+    default_crime = dash_duo.find_element(By.XPATH, "/html/body/div/div/div/div[3]/div[1]/div/div/div[2]/div[1]/div[3]/div/div/div/div[1]").text
+    assert default_crime.text() == 'Burglary'.text()
+
+
+
+
+
 
 
 
