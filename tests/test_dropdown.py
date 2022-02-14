67d775b3fb7ba717295f12dbf628038558a96d05
weirdaxe <matic.potocnik1@gmail.com>
2022-02-14 15:58:48 +0000

    changed to more basic

--- tests/test_dropdown.py
+++ tests/test_dropdown.py
@@ -3,4 +3,4 @@ from selenium.webdriver.support.ui import WebDriverWait
 def test_default_map(dash_duo):
     dash_duo.wait_for_element("h1", timeout=4)
     WebDriverWait(dash_duo.driver, 3)
-    assert 'Raw' in dash_duo.find_element("#data_select").text, "'Raw' should appear in the data select dropdown"
+    assert 'Display Settings' in dash_duo.find_element("#display_settings").text
