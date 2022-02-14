678aa514e89b15c9a3ee75ef34fcc5e4a814480e
weirdaxe <matic.potocnik1@gmail.com>
2022-02-14 16:03:20 +0000

    commit

--- tests/test_dropdown.py
+++ tests/test_dropdown.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 
 def test_default_map(dash_duo):
-    dash_duo.wait_for_element("h1", timeout=4)
+    dash_duo.wait_for_element("display_settings", timeout=4)
     WebDriverWait(dash_duo.driver, 3)
     assert 'Display Settings' in dash_duo.find_element("display_settings").text
