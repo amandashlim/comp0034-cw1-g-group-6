f043cb32f3554c725f5ab6d2aaac6886137d4643
weirdaxe <matic.potocnik1@gmail.com>
2022-02-14 17:04:33 +0000

    changed

--- tests/test_dropdown.py
+++ tests/test_dropdown.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from dash.testing.application_runners import import_app
 
-def test_default_map(dash_duo):
-    app = import_app("app.crime_app")
+
+def test_crda001_H1_text_equals(dash_duo):
+    app = import_app(app_file="app.crime_app")
     dash_duo.start_server(app)
-    dash_duo.wait_for_element("display_settings", timeout=60)
-    WebDriverWait(dash_duo.driver, 3)
-    assert 'Display Settings' in dash_duo.find_element("display_settings").text
+    dash_duo.wait_for_element("H2", timeout=10)
+    h1_text = dash_duo.find_element("#crime_select_text").text
+    print(h1_text)
+    assert h1_text.casefold() == "Select Crime to Display".casefold()
