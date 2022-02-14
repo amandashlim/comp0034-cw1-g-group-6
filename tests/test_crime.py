ad9ab16ff2f267493a19df25c53805b81fc13756
weirdaxe <matic.potocnik1@gmail.com>
2022-02-14 15:34:26 +0000

    changed dash_app to crime_app bc of main file changes

--- tests/test_crime.py
+++ tests/test_crime.py
@@ -8,7 +8,7 @@ def test_crda001_H1_text_equals(dash_duo):
     WHEN we access the homepage of the app
     THEN the H1 heading should say "London Crime Rates"""
 
-    app = import_app(app_file = "crime_app.dash_app")
+    app = import_app(app_file = "crime_app.crime_app")
     dash_duo.start_server(app)
     dash_duo.wait_for_element("H1", timeout = 4)
     h1_text = dash_duo.find_element("H1").text
@@ -21,7 +21,7 @@ def test_crda002_show_settings(dash_duo):
     THEN there should be a paragraph with Select Chart Type
     """
 
-    app = import_app(app_file = "crime_app.dash_app")
+    app = import_app(app_file = "crime_app.crime_app")
     dash_duo.start_server(app)
     dash_duo.wait_for_element("H1", timeout = 4)
     filter_selector = dash_duo.find_element("H3").text
@@ -32,7 +32,7 @@ def test_crda003_default_crime(dash_duo):
     WHEN look at the Select Crime to Display filter
     THEN the default crime should be Burglary"""
 
-    app = import_app(app_file="crime_app.dash_app")
+    app = import_app(app_file="crime_app.crime_app")
     dash_duo.start_server(app)
     dash_duo.wait_for_element("H1", timeout=4)
     WebDriverWait(dash_duo.driver, 3)
