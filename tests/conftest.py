f47944512fc923f8b78e91a5289c961b73497020
weirdaxe <matic.potocnik1@gmail.com>
2022-02-15 14:12:13 +0000

    headless

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ def pytest_setup_options():
     options = Options()
     # Uncomment the following if testing on GitHub actions, the browser needs to run in headless mode
     options.add_argument('--disable-gpu')
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     return options
 
 '''
