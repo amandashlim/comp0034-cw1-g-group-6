2bc9782d35bb5de860855a8dcbaa5567931ac55b
weirdaxe <matic.potocnik1@gmail.com>
2022-02-16 01:44:35 +0000

    updated fucntions with fixtures

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -9,6 +9,7 @@ def test_imported_data():
     When the class is called
     Then the class should get the data automatically
     '''
+
     assert v.df.shape == (288, 26)
 
 
@@ -18,78 +19,59 @@ def test_reformating_shape():
     When reformat function is used
     Then the function should return the correct datashape
     '''
+
     assert v.reformat(v.df).shape == (768, 13)
 
 
-def test_reformating_columns():
+def test_reformating_columns(reformat_columns):
     '''
     Given the class is called
     When reformat function is used
     Then the function should return the data with specific columns
     '''
 
-    c = ['Borough', 'Date', 'Burglary', 'Criminal Damage', 'Drugs',
-         'Fraud or Forgery', 'Other Notifiable Offences', 'Robbery',
-         'Sexual Offences', 'Theft and Handling', 'Violence Against the Person',
-         'Total Crime', 'Average Crime']
-    assert list(v.reformat(v.df).columns) == c
+    assert list(v.reformat(v.df).columns) == reformat_columns
 
 
-def test_crime_list():
+def test_crime_list(crime_list):
     '''
     Given the class is called
     When the class is called
     Then the function should return the list of crimes
     '''
 
-    crimes = ['Burglary', 'Criminal Damage', 'Drugs',
-              'Fraud or Forgery', 'Other Notifiable Offences', 'Robbery',
-              'Sexual Offences', 'Theft and Handling', 'Violence Against the Person']
-    assert list(v.crime_list) == crimes
+    assert list(v.crime_list) == crime_list
 
 
-def test_borough_list():
+def test_borough_list(borough_list):
     '''
     Given the class is called
     When the class is called
     Then the function should return the list of boroughs
     '''
 
-    boroughs = ['Barking and Dagenham', 'Barnet', 'Bexley', 'Brent', 'Bromley', 'Camden',
-                'Croydon', 'Ealing', 'Enfield', 'Greenwich', 'Hackney',
-                'Hammersmith and Fulham', 'Haringey', 'Harrow', 'Havering', 'Hillingdon',
-                'Hounslow', 'Islington', 'Kensington and Chelsea', 'Kingston upon Thames',
-                'Lambeth', 'Lewisham', 'Merton', 'Newham', 'Redbridge', 'Richmond upon Thames',
-                'Southwark', 'Sutton', 'Tower Hamlets', 'Waltham Forest', 'Wandsworth',
-                'Westminster']
-    assert list(v.borough_list) == boroughs
+    assert list(v.borough_list) == borough_list
 
 
-def test_date_list():
+def test_date_list(date_list):
     '''
     Given the class is called
     Whem the class is called
     Then the function should return the list of dates
     '''
 
-    dates = ['201910', '201911', '201912', '202001',
-             '202002', '202003', '202004', '202005',
-             '202006', '202007', '202008', '202009',
-             '202010', '202011', '202012', '202101',
-             '202102', '202103', '202104', '202105',
-             '202106', '202107', '202108', '202109']
-    assert list(v.date_list) == dates
+    assert list(v.date_list) == date_list
 
 
-def test_get_highlights():
+def test_get_highlights(borough_list, geo_borough_dict):
     '''
     Given the class is called
     When the selections are passed into get_highlights
     Then the function should return the geojson file for those selections (boroughs)
     '''
-
-    subgeo = v.geo['features'][25]['properties']
-    assert v.get_highlights(["Camden"])['features'][0]['properties'] == subgeo
+    for i in borough_list:
+        subgeo = geo_borough_dict[i]
+        assert v.get_highlights([i])['features'][0]['properties'] == subgeo
 
 
 def test_map_statistics():
