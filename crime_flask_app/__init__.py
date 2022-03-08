f1170ba0879f46c8e100d005976f4085e3895ce6
axe <zceimpo@ucl.ac.uk>
2022-03-08 13:14:45 +0000

    added dash app

---
+++ crime_flask_app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+def create_app():
+    """
+    Initialise the Flask application.
+    :rtype: Returns a configured Flask object
+    """
+    app = Flask(__name__)
+    with app.app_context():
+        # Import Dash application
+        from crime_dash_app.crime_app import init_dashboard
+        app = init_dashboard(app)
+
+    return app
