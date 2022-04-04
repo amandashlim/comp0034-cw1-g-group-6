e1248d5fd8f786164ec43a499826a7304c0064c5
weirdaxe <matic.potocnik1@gmail.com>
2022-04-04 21:10:41 +0200

    added fixture

--- crime_flask_app/tests/conftest.py
+++ crime_flask_app/tests/conftest.py
@@ -1,6 +1,10 @@
+import multiprocessing
+
 import pytest
-from crime_flask_app.models import User, Post, Comment
-from crime_flask_app import create_app, db, config
+from selenium.webdriver import Chrome, ChromeOptions
+
+from crime_flask_app import create_app, db
+from crime_flask_app.models import User, Post
 
 
 @pytest.fixture(scope='session')
@@ -38,6 +42,35 @@ def app_paths_no_login():
 def app_paths_login(username="pepe1"):
     app_paths = ["/dashboard","/blog","/create_post",f"/{username}",f"posts/{username}"]
     yield app_paths
+
+
+@pytest.fixture(scope='class')
+def chrome_driver(request):
+    """ Selenium webdriver with options to support running in GitHub actions
+    Note:
+        For CI: `headless` and `disable-gpu` not commented out
+        For running on your computer: `headless` and `disable-gpu` to be commented out
+    """
+    options = ChromeOptions()
+    options.add_argument("--headless")  # use for GitHub Actions CI
+    options.add_argument('--disable-gpu') # use for GitHub Actions CI
+    options.add_argument("--window-size=1920,1080")
+    chrome_driver = Chrome(options=options)
+    request.cls.driver = chrome_driver
+    yield
+    chrome_driver.close()
+
+
+@pytest.fixture(scope='class')
+def run_app(app):
+    """
+    Fixture to run the Flask app for Selenium browser tests
+    """
+    multiprocessing.set_start_method("fork")  # Needed in Python 3.8 and later
+    process = multiprocessing.Process(target=app.run, args=())
+    process.start()
+    yield process
+    process.terminate()
 
 '''
 @pytest.fixture(scope='session')
