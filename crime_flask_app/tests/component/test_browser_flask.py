9693c6fa31cc59ba00468b313b52e97033030055
amandashlim <amandashlim1@gmail.com>
2022-04-06 10:47:29 +0100

    Begin username test and add ID's to things in my account

--- crime_flask_app/tests/component/test_browser_flask.py
+++ crime_flask_app/tests/component/test_browser_flask.py
@@ -306,7 +306,7 @@ class Test1:
 
     def test_existing_user_liking_a_post(self):
         """Test when an existing user: opens the website it loads correctly,
-                                   loges in successfully,
+                                   logs in successfully,
                                    views blog posts
                                    likes the second post
                                    the like counter changes
@@ -349,5 +349,40 @@ class Test1:
         self.driver.get('http://127.0.0.1:5000/logout')
         self.driver.implicitly_wait(5)
 
+    def test_username_updates(self):
+        """Test that a user can log in, enter in a new username through the form and their username will update"""
+        # Go to the home page
+        self.driver.get('http://127.0.0.1:5000/')
+
+        # Click login button
+        # See https://www.selenium.dev/documentation/webdriver/waits/
+        self.driver.implicitly_wait(5)
+        self.driver.find_element(By.ID, "nav-login-btn").click()
+
+        # Test person data
+        email = "pepe1@gmail.com"
+        password = "123456"
+
+        # Fill in login form
+        self.driver.find_element(By.ID, "email").send_keys(email)
+        self.driver.find_element(By.ID, "password").send_keys(password)
+        self.driver.find_element(By.ID, "btn-login").click()
+
+        # Assert that browser redirects to index page
+        self.driver.implicitly_wait(10)
+        assert self.driver.current_url == 'http://127.0.0.1:5000/home'
+
+        # Goto my account page for pepe1
+        self.driver.implicitly_wait(5)
+        self.driver.find_element(By.ID, "my_account-btn").click()
+        assert self.driver.current_url == 'http://127.0.0.1:5000/pepe1'
+
+        # Go to update profile page
+        self.driver.implicitly_wait(5)
+        self.driver.find_element(By.ID, "update-profile-btn").click()
+        assert self.driver.current_url == 'http://127.0.0.1:5000/update/1'
+
+    # def test_email_updates(self):
+        # test test test
 def document_initialised(driver):
     return driver.execute_script("return initialised")
